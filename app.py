35c29b7bd63ac2357303ef3b657484d9a6d6aeb1
Joshua Robison <joshua@paperspace.com>
2022-04-01 16:20:05 -0700

    Updating deployment to use style_transfer_folder.py

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from PIL import Image
 import torch
 import os
 import subprocess
-import generate_image
+import style_transfer_folder
+# import generate_image
 
 st.title("ResNet + Streamlit Classification")
 
 uploaded_file = st.file_uploader("Choose an image...", type=['png','jpeg'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
+
+    # for files in os.listdir("dataset3"):
+    # if os.path.exists("./uploaded_img"):
+    #     os.system("rm -rf "+"./uploaded_img")
+
+    # cv2.imwrite(f'{args.outdir}/{str(i).zfill(6)}.jpg', out)
+    
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Generating...")
 
+    # !python style_transfer_folder.py --size 1024 --ckpt ./pretrained_models/blendgan.pt --psp_encoder_ckpt ./pretrained_models/psp_encoder.pt --style_img_path ./test_imgs/jr_style_imgs/ --input_img_path ./test_imgs/face_imgs/ --outdir results/jr_style_transfer/
     
-    generated_img = generate_image.run(size=1024, pics=1, ckpt='/opt/models/pretrained_models/blendgan.pt', style_img='./test_imgs/jr_style_imgs/abstract_image_1017.jpeg', outdir='./results/generated_image/jr_test/')
-
+    # generated_img = generate_image.run(size=1024, pics=1, ckpt='/opt/models/pretrained_models/blendgan.pt', style_img='./test_imgs/jr_style_imgs/abstract_image_1017.jpeg', outdir='./results/generated_image/jr_test/')
+    generated_img = style_transfer_folder.run(size=1024, ckpt='/opt/models/pretrained_models/blendgan.pt', psp_encoder_ckpt='/opt/models/pretrained_models/psp_encoder.pt', style_img_path='./style_img', img_in=image)
     st.image(generated_img)
--- style_transfer_folder.py
+++ style_transfer_folder.py
@@ -18,6 +18,80 @@ np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 
+# !python style_transfer_folder.py --size 1024 --ckpt ./pretrained_models/blendgan.pt --psp_encoder_ckpt ./pretrained_models/psp_encoder.pt --style_img_path ./test_imgs/jr_style_imgs/ --input_img_path ./test_imgs/face_imgs/ --outdir results/jr_style_transfer/
+
+def run(size, ckpt, psp_encoder_ckpt, style_img_path, img_in):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--size', type=int, default=1024)
+
+    parser.add_argument('--ckpt', type=str, default='', help='path to BlendGAN checkpoint')
+    parser.add_argument('--psp_encoder_ckpt', type=str, default='', help='path to psp_encoder checkpoint')
+
+    parser.add_argument('--style_img_path', type=str, default=None, help='path to style image')
+    parser.add_argument('--input_img_path', type=str, default=None, help='path to input image')
+    parser.add_argument('--add_weight_index', type=int, default=6)
+
+    parser.add_argument('--channel_multiplier', type=int, default=2)
+
+    args = parser.parse_args()
+
+    args.size = size
+    args.ckpt = ckpt
+    args.psp_encoder_ckpt = psp_encoder_ckpt
+    args.style_img_path = style_img_path
+    args.input_img_path = input_img_path
+
+    args.latent = 512
+    args.n_mlp = 8
+
+    checkpoint = torch.load(args.ckpt)
+    model_dict = checkpoint['g_ema']
+    print('ckpt: ', args.ckpt)
+
+    g_ema = Generator(
+        args.size, args.latent, args.n_mlp, channel_multiplier=args.channel_multiplier, load_pretrained_vgg=False
+    ).to(device)
+    g_ema.load_state_dict(model_dict)
+    g_ema.eval()
+
+    psp_encoder = PSPEncoder(args.psp_encoder_ckpt, output_size=args.size).to(device)
+    psp_encoder.eval()
+
+    input_img_paths = sorted(glob.glob(os.path.join(args.input_img_path, '*.*')))
+    style_img_paths = sorted(glob.glob(os.path.join(args.style_img_path, '*.*')))[:]
+
+
+
+    # name_in = os.path.splitext(os.path.basename(input_img_path))[0]
+    # img_in = cv2.imread(input_img_path, 1)
+    img_in_ten = cv2ten(img_in, device)
+    img_in = cv2.resize(img_in, (args.size, args.size))
+
+    for style_img_path in style_img_paths:
+        name_style = os.path.splitext(os.path.basename(style_img_path))[0]
+        img_style = cv2.imread(style_img_path, 1)
+        img_style_ten = cv2ten(img_style, device)
+        img_style = cv2.resize(img_style, (args.size, args.size))
+
+        with torch.no_grad():
+            sample_style = g_ema.get_z_embed(img_style_ten)
+            sample_in = psp_encoder(img_in_ten)
+            img_out_ten, _ = g_ema([sample_in], z_embed=sample_style, add_weight_index=args.add_weight_index,
+                                    input_is_latent=True, return_latents=False, randomize_noise=False)
+            img_out = ten2cv(img_out_ten)
+        
+        
+
+        # out = np.concatenate([img_in, img_style, img_out], axis=1)
+        out = img_out
+        break
+        # cv2.imwrite(f'{args.outdir}/{name_in}_v_{name_style}.jpg', out)
+
+    return out
+    # print('Done!')
 
 if __name__ == '__main__':
     
