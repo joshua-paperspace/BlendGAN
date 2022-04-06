62243437b6204597c048183a20e449bc89bd07ac
Joshua Robison <joshua@paperspace.com>
2022-04-06 15:15:02 -0700

    Changed image upload type

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.title("ResNet + Streamlit Classification")
 
 uploaded_file = st.file_uploader("Choose an image...", type=['png','jpeg'])
 if uploaded_file is not None:
-    # image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file)
     # img_in = cv2.imread(input_img_path, 1)
     # image = cv2.imdecode(np.fromstring(uploaded_file, np.uint8), cv2.IMREAD_UNCHANGED)
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, 1)
+    img_in = cv2.imdecode(file_bytes, 1)
 
     # for files in os.listdir("dataset3"):
     # if os.path.exists("./uploaded_img"):
@@ -32,5 +32,5 @@ if uploaded_file is not None:
     # !python style_transfer_folder.py --size 1024 --ckpt ./pretrained_models/blendgan.pt --psp_encoder_ckpt ./pretrained_models/psp_encoder.pt --style_img_path ./test_imgs/jr_style_imgs/ --input_img_path ./test_imgs/face_imgs/ --outdir results/jr_style_transfer/
     
     # generated_img = generate_image.run(size=1024, pics=1, ckpt='/opt/models/pretrained_models/blendgan.pt', style_img='./test_imgs/jr_style_imgs/abstract_image_1017.jpeg', outdir='./results/generated_image/jr_test/')
-    generated_img = style_transfer_folder.run(size=1024, ckpt='/opt/models/pretrained_models/blendgan.pt', psp_encoder_ckpt='/opt/models/pretrained_models/psp_encoder.pt', style_img_path='./style_img', img_in=image)
+    generated_img = style_transfer_folder.run(size=1024, ckpt='/opt/models/pretrained_models/blendgan.pt', psp_encoder_ckpt='/opt/models/pretrained_models/psp_encoder.pt', style_img_path='./style_img', img_in=img_in)
     st.image(generated_img)
